eab61d548c940790beeafed1531c57d0a7132697
alantan <34753877+AlanyTan@users.noreply.github.com>
2025-02-06 09:36:06 -0500

    Create quartet.py

---
+++ SameProductQuartet/quartet.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def tupleSameProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+def test():
+    """
+    >>> spq = Solution()
+    >>> spq.tupleSameProduct((1,2,4,5,10))
+    16
+    >>> spq.tupleSameProduct((2,3,4,6))
+    8
+    """
+    
+
+    
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
+                     
